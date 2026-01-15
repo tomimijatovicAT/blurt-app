161ad0d0d2f726210b4a24120d0176d27fe351f5
tomi <66423466+tomimijatovicAT@users.noreply.github.com>
2026-01-15 22:13:58 +0100

    Initial Commit

---
+++ Notable/NotableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotableApp.swift
+//  Notable
+//
+//  Created by Tomislav Mijatovic on 15.01.26.
+//
+
+import SwiftUI
+
+@main
+struct NotableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
